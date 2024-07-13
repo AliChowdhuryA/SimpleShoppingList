7afb9d9f63f0a46a56ff69b264aed51bcb0da4d7
AliChowdhuryA <Alichowdhurya@gmail.com>
2024-07-13 17:49:10 -0400

    added hard coded list

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -21,11 +21,29 @@ class MyApp extends StatelessWidget
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        color: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          
+        ),
+        body: ListView
+        (
+          children: const <Widget>[
+            ListTile(
+              // Dropdown arrow icon
+              leading: Icon(Icons.arrow_drop_down_circle),
+              title: Text('Item 1'),
+            )
+
+          ],
+        ),
+      
         
       ),
+      
+      
     );
+    
 }
 }
   
