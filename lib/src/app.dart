4fdb2515e133e2c7b91c0b1377e6088b19c65f2b
AliChowdhuryA <Alichowdhurya@gmail.com>
2024-07-17 17:45:25 -0400

    dependencies

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
+import '../pages/home.dart';
+
 
 /// The Widget that configures your application.
 
@@ -16,32 +12,8 @@ class MyApp extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
-          
-        ),
-        body: ListView
-        (
-          children: const <Widget>[
-            ListTile(
-              // Dropdown arrow icon
-              leading: Icon(Icons.arrow_drop_down_circle),
-              title: Text('Item 1'),
-            )
-
-          ],
-        ),
-      
-        
-      ),
-      
-      
+    return const MaterialApp(
+      home: HomePage(),
     );
     
 }
